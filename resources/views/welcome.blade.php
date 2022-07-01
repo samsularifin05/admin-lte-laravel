4b9ea01e859a1ddcfd3ea4d03ab7743a226513d3
samsularifin05 <samsul026@gmail.com>
2022-07-01 11:24:17 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="base_url" content="{{ url('') }}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Iventory</title>
+    <title>laravel-react-admin-lte</title>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
